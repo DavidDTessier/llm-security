2029604debf0dfe790c69d88980a5089fd2b045f
DavidDTessier <57716152+DavidDTessier@users.noreply.github.com>
2025-03-07 18:53:16 -0500

    Updates
    added requirements.txt and fixed code issues

--- infra/setup.sh
+++ infra/setup.sh
@@ -13,9 +13,15 @@ source .config
 echo $GCP_Org_ID
 echo $GCP_Project_ID
 echo $GCP_Org_Domain
-echo $Region
+echo $GCP_Region
 
-model_armor_url = "modelarmor.$Region.rep.googleapis.com"
+model_armor_url=""
+
+if [ $GCP_Region = "global" ]; then
+    model_armor_url="modelarmor.googleapis.com"
+else
+    model_armor_url="modelarmor.$GCP_Region.rep.googleapis.com"
+fi
 
 echo $model_armor_url
 
@@ -26,16 +32,7 @@ gcloud auth login
 gcloud config set project $GCP_Project_ID
 
 # Set the API Endpoint for the Model Armor Service
-gcloud config set api_endpoint_overrides/modelarmor $model_armor_url
+gcloud config set api_endpoint_overrides/modelarmor "https://$model_armor_url/"
 
 # Enable Model Armor on the GCP Project
 gcloud services enable modelarmor.googleapis.com --project $GCP_Project_ID
-
-
-
-
-
-
-
-
-# Authentication 
